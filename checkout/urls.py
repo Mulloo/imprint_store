332cb7ae80fcbe99da9efa2f75ad168bc4fae5f4
Daniel Mullooly <mulloolyd.1998@gmail.com>
2024-10-01 23:04:49 +0000

    added checkout form data caching in payment intent

--- checkout/urls.py
+++ checkout/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
         name='checkout_success'
         ),
     path('wh/', webhook, name='webhook'),
+    path(
+        'cache_checkout_data/', views.cache_checkout_data,
+        name='cache_checkout_data'
+        ),
 ]

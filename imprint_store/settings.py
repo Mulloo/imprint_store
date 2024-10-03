da17cb05bd3e40754099aef13746ff2bca41767d
Daniel Mullooly <mulloolyd.1998@gmail.com>
2024-10-03 02:57:02 +0000

    fix: Email_backend typo

--- imprint_store/settings.py
+++ imprint_store/settings.py
@@ -188,7 +188,7 @@ if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'imprintstore@example.com'
 else:
-    EMAIL_BCKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
--- products/admin.py
+++ products/admin.py
@@ -10,7 +10,6 @@ class ProductAdmin(admin.ModelAdmin):
         'price',
         'rating',
         'image',
-        'tags',
     )
 
     ordering = ('sku',)

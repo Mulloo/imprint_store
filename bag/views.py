c008109ad1159184f754040b4cb829a6fb0cf7cc
Mulloo <mulloolyd.1998@gmail.com>
2025-06-14 06:39:26 +0100

    fix: issue in bag view.py

--- bag/views.py
+++ bag/views.py
@@ -38,8 +38,7 @@ def add_to_bag(request, item_id):
             else:
                 bag[item_id]['items_by_size'][size] = quantity
                 messages.success(
-                    request, f'Added size {size.upper(
-                    )} {product.name} to your bag')
+                    request, f'Added size {size.upper()} {product.name} to your bag')
         else:
             bag[item_id] = {'items_by_size': {size: quantity}}
             messages.success(

4c5eadb92e34874305f634a967f164bc7b559898
Mulloo <mulloolyd.1998@gmail.com>
2025-06-23 19:41:48 +0100

    change: removed admin page many to many in list

--- wishlist/admin.py
+++ wishlist/admin.py
@@ -7,6 +7,5 @@ class WishlistAdmin(admin.ModelAdmin):
     """ Wishlist admin """
     list_display = ['user']
     search_fields = ['user__username']
-    readonly_fields = ['created_at', 'updated_at']
-    
+
 

2e185cb5e6d1d8d9115b40248a042937554ac130
Mulloo <mulloolyd.1998@gmail.com>
2025-06-23 18:42:05 +0100

    fix: removed django.extensions form settings.py used to generate erd.png

--- imprint_store/settings.py
+++ imprint_store/settings.py
@@ -32,7 +32,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-    'django_extensions',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',

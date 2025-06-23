89ae787f20723030a1865ee2587415098293944c
Mulloo <mulloolyd.1998@gmail.com>
2025-06-23 21:17:17 +0100

    fix: added redirect for /favicon urls.py

--- imprint_store/urls.py
+++ imprint_store/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include , re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 from imprint_store.views import handler404
 
@@ -15,6 +16,9 @@ urlpatterns = [
     path('profiles/', include('profiles.urls')),
     path('wishlist/', include('wishlist.urls')),
 
+
+    re_path(r'^favicon\.ico$', RedirectView.as_view(
+        url=settings.STATIC_URL + 'favicon.ico', permanent=True)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = handler404

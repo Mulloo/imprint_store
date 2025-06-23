9d95b716ba6a1c6034e73f8dc5836deeb7ed5563
Mulloo <mulloolyd.1998@gmail.com>
2025-06-23 19:09:10 +0100

    fix:categorys for all colthing updated

--- wishlist/admin.py
+++ wishlist/admin.py
@@ -7,3 +7,6 @@ class WishlistAdmin(admin.ModelAdmin):
     """ Wishlist admin """
     list_display = ['user']
     search_fields = ['user__username']
+    readonly_fields = ['created_at', 'updated_at']
+    
+

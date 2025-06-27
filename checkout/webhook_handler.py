d9a2505bdc94d588437b22afdd4b3e586e57a720
Mulloo <mulloolyd.1998@gmail.com>
2025-06-27 14:43:29 +0100

    test: confirm email for orders testing

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -33,7 +33,7 @@ class StripeWH_Handler:
             subject,
             body,
             settings.DEFAULT_FROM_EMAIL,
-            [cust_email]
+            [cust_email],fail_silently=False,
         )
 
     def handle_event(self, event):
